4870f0adebf92452598630ffc64f75cf31ea94e2
wangrq <98709484@qq.com>
2020-08-27 23:20:56 +0800

    fix

--- src/main/java/com/example/demo/leetcode/CenerateParenthesis.java
+++ src/main/java/com/example/demo/leetcode/CenerateParenthesis.java
@@ -1,11 +1,9 @@
 package com.example.demo.leetcode;
 
 import org.junit.Test;
+import sun.util.calendar.ZoneInfoFile;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @ClassName CenerateParenthesis
@@ -14,28 +12,62 @@ import java.util.List;
  * @Date 2020/8/26 17:40
  */
 public class CenerateParenthesis {
+    char left = '(';
+    char right = ')';
+
     public List<String> solution(int n) {
-        String left = "(";
-        String right = ")";
-
-        StringBuilder sb;
-        if (n == 1) {
-            return new ArrayList<>(Collections.singletonList(left + right));
-        } else {
-            sb = new StringBuilder();
-            for (int i = 0; i < n; i++) {
-                sb.append(left).append(right);
-            }
-
+        if (n == 0) {
+            return null;
         }
-        // TODO: 2020/8/26 还未解决
-        return null;
+        ArrayList<String> list = new ArrayList<>();
+        list.add("(");
+        return recursion(list, 1, n - 1, n * 2 - 1);
+    }
+
+    public List<String> recursion(List<String> list, int rightNum, int leftNum, int n) {
+        List<String> res = new ArrayList<>(list.size());
+        List<String> res2 = new ArrayList<>(list.size());
+        if (n == 1) {
+            for (String val : list) {
+                res.add(val + right);
+            }
+            return res;
+        } else {
+            if (rightNum == 0) {
+                for (String val : list) {
+                    res.add(val + left);
+                }
+                return recursion(res, rightNum + 1, leftNum - 1, n - 1);
+            } else {
+                HashSet<String> recursion = new HashSet<>();
+                if (leftNum > 0) {
+                    for (String val : list) {
+                        res.add(val + left);
+                    }
+                    recursion = new HashSet<>(recursion(res, rightNum + 1, leftNum - 1, n - 1));
+                }
+                for (String val : list) {
+                    res2.add(val + right);
+                }
+                List<String> recursion2 = recursion(res2, rightNum - 1, leftNum, n - 1);
+                recursion.addAll(recursion2);
+                return new ArrayList<>(recursion);
+            }
+        }
+
+
     }
 
     @Test
     public void test() {
-        List<String> list = new ArrayList<>(Arrays.asList("((()))", "(()())", "(())()", "()(())", "()()()"));
-        List<String> solution = solution(3);
+        List<String> list = new ArrayList<>(Arrays.asList("(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"));
+        List<String> solution = solution(4);
+        System.out.println(solution);
+        for (String s : list) {
+            if (!solution.contains(s)) {
+                System.err.println(s);
+            }
+        }
         assert list.equals(solution);
 
     }
---
+++ src/main/java/com/example/demo/leetcode/FindItinerary.java
@@ -0,0 +1,121 @@
+package com.example.demo.leetcode;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName FindItinerary
+ * @Description TODO
+ * @Author wangrq
+ * @Date 2020/8/27 9:07
+ */
+public class FindItinerary {
+
+    HashMap<String, ArrayList<String>> journey;
+    LinkedList<HashMap<String, ArrayList<String>>> properlyMap = new LinkedList<>();
+    LinkedList<ArrayList<String>> properlyList = new LinkedList<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        journey = new HashMap<>(tickets.size());
+        String val, key;
+        for (List<String> ticket : tickets) {
+            val = ticket.get(1);
+            key = ticket.get(0);
+            ArrayList<String> strings = journey.get(key);
+            if (Objects.isNull(strings)) {
+                strings = new ArrayList<>();
+                strings.add(val);
+            } else {
+                strings.add(val);
+                Collections.sort(strings);
+            }
+            journey.put(key, strings);
+        }
+        ArrayList<String> res = new ArrayList<>();
+        String start = "JFK";
+        res.add(start);
+        List<String> doit = doit(res, journey, start);
+        while (Objects.isNull(doit)) {
+            ArrayList<String> list = properlyList.pop();
+            HashMap<String, ArrayList<String>> map = properlyMap.pop();
+            start = list.get(list.size() - 1);
+            doit = doit(list, map, start);
+        }
+        return doit;
+    }
+
+    public List<String> doit(ArrayList<String> res, HashMap<String, ArrayList<String>> map, String start) {
+        HashMap<String, ArrayList<String>> copy = new HashMap<>(map);
+        while (!map.isEmpty()) {
+            ArrayList<String> list = map.get(start);
+            if (Objects.isNull(list)) {
+                return null;
+            }
+            if (list.size() == 1) {
+                map.remove(start);
+                start = list.get(0);
+                res.add(start);
+            } else {
+                String pop;
+                LinkedList<String> cpList = new LinkedList<>(list);
+                List<String> cpRes = new ArrayList<>(res);
+                String cpStart = start;
+                boolean check = false;
+                for (int i = 0; i < cpList.size(); i++) {
+                    if (copy.containsKey(cpList.get(i))) {
+                        if (!check) {
+                            pop = list.remove(i);
+                            map.put(start, list);
+                            start = pop;
+                            res.add(start);
+                            check = true;
+                        } else {
+                            ArrayList<String> cpList2 = new ArrayList<>(cpList);
+                            Map<String, ArrayList<String>> copyMap;
+                            copyMap = copy.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> new ArrayList<>(e.getValue())));
+                            ArrayList<String> cpRes2 = new ArrayList<>(cpRes);
+                            pop = cpList2.remove(i);
+                            cpRes2.add(pop);
+                            copyMap.put(cpStart, cpList2);
+                            properlyList.push(cpRes2);
+                            properlyMap.push(new HashMap<>(copyMap));
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        List<List<String>> tickets = new ArrayList<>();
+        tickets.add(new ArrayList<>(Arrays.asList("EZE", "TIA")));
+        tickets.add(new ArrayList<>(Arrays.asList("EZE", "HBA")));
+        tickets.add(new ArrayList<>(Arrays.asList("AXA", "TIA")));
+        tickets.add(new ArrayList<>(Arrays.asList("JFK", "AXA")));
+        tickets.add(new ArrayList<>(Arrays.asList("ANU", "JFK")));
+        tickets.add(new ArrayList<>(Arrays.asList("ADL", "ANU")));
+        tickets.add(new ArrayList<>(Arrays.asList("TIA", "AUA")));
+        tickets.add(new ArrayList<>(Arrays.asList("ANU", "AUA")));
+        tickets.add(new ArrayList<>(Arrays.asList("ADL", "EZE")));
+        tickets.add(new ArrayList<>(Arrays.asList("ADL", "EZE")));
+        tickets.add(new ArrayList<>(Arrays.asList("EZE", "ADL")));
+        tickets.add(new ArrayList<>(Arrays.asList("AXA", "EZE")));
+        tickets.add(new ArrayList<>(Arrays.asList("AUA", "AXA")));
+        tickets.add(new ArrayList<>(Arrays.asList("JFK", "AXA")));
+        tickets.add(new ArrayList<>(Arrays.asList("AXA", "AUA")));
+        tickets.add(new ArrayList<>(Arrays.asList("AUA", "ADL")));
+        tickets.add(new ArrayList<>(Arrays.asList("ANU", "EZE")));
+        tickets.add(new ArrayList<>(Arrays.asList("TIA", "ADL")));
+        tickets.add(new ArrayList<>(Arrays.asList("EZE", "ANU")));
+        tickets.add(new ArrayList<>(Arrays.asList("AUA", "ANU")));
+        List<String> solution = findItinerary(tickets);
+        List<String> res = new ArrayList<>(Arrays.asList("JFK", "AXA", "AUA", "ADL", "ANU", "AUA", "ANU", "EZE", "ADL", "EZE", "ANU", "JFK", "AXA", "EZE", "TIA", "AUA", "AXA", "TIA", "ADL", "EZE", "HBA"));
+//        res = new ArrayList<>(Arrays.asList("JFK", "AXA", "AUA", "ADL", "ANU", "AUA", "ANU", "EZE", "ADL", "EZE", "TIA", "AUA", "AXA", "EZE", "ANU", "JFK", "AXA", "TIA", "ADL", "EZE", "HBA"));
+        System.out.println(solution);
+        assert solution.equals(res);
+    }
+}
