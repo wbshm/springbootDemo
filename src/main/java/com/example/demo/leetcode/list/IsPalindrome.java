4e820efc800ac83f3719d9bcd0a9a316e5d26675
wangrq <wrq142857@gmail.com>
2020-10-23 12:48:58 +0800

    fix

--- src/main/java/com/example/demo/leetcode/list/IsPalindrome.java
+++ src/main/java/com/example/demo/leetcode/list/IsPalindrome.java
@@ -2,8 +2,22 @@ package com.example.demo.leetcode.list;
 
 public class IsPalindrome {
     public boolean isPalindrome(ListNode head) {
-
-
-        return false;
+        ListNode pre = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            ListNode temp = slow.next;
+            if(pre != null) slow.next = pre;
+            pre = slow;
+            fast = fast.next.next;
+            slow = temp;
+        }
+        if(fast != null) slow = slow.next;
+        while(slow != null){
+            if(slow.val != pre.val) return false;
+            slow = slow.next;
+            pre = pre.next;
+        }
+        return true;
     }
 }
